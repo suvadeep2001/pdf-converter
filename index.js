994f51d07381ab391bbb293626c85577e652f457
Suvadeep Maity <68159874+suvadeep2001@users.noreply.github.com>
2021-08-05 21:37:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
 const path = require("path");
 
-const { exec } = require('child_process')
+const {exec} = require('child_process')
 
 const outputFilePath = Date.now() + "output.pdf"
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
-
-
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.use(express.static("public"));
 
 var list = ""
 
+const fs = require('fs')
 
-const fs = require('fs');
+const multer = require('multer')
 
-const multer = require('multer');
-const { stdout, stderr } = require('process');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "public/uploads");
+      cb(null, "public/uploads");
     },
     filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-        );
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
     },
-});
-
-const imageFilter = function (req, file, cb) {
+  });
+  
+  const imageFilter = function (req, file, cb) {
     if (
-        file.mimetype == "image/png" ||
-        file.mimetype == "image/jpg" ||
-        file.mimetype == "image/jpeg"
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
     ) {
-        cb(null, true);
+      cb(null, true);
     } else {
-        cb(null, false);
-        return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
     }
-};
-
-var upload = multer({ storage: storage, fileFilter: imageFilter });
+  };
+  
+  var upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const PORT = process.env.PORT || 3000
 
@@ -59,39 +55,45 @@ var dir = "public";
 var subDirectory = "public/uploads";
 
 if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+  fs.mkdirSync(dir);
 
-    fs.mkdirSync(subDirectory);
+  fs.mkdirSync(subDirectory);
 }
-app.get("/", (req, res) => {
-    res.sendfile(__dirname + "/index.html");
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/index.html")
 })
 
-app.post("/merge", upload.array("files", 1000), (req, res) => {
-    list = "";
-    if (req.files) {
-        req.files.forEach((file) => {
-            list += `${file.path}`;
-            list += " ";
+app.post('/merge',upload.array('files',100),(req,res) => {
+    list = ""
+    if(req.files){
+        req.files.forEach(file => {
+            console.log(file.path)
+
+            list+= `${file.path}`
+            list+= " "
         });
 
-        console.log(list);
+        console.log(list)
 
-        exec(`magick convert ${list} ${outputFilePath}`, (err, stderr, stdout) => {
-            if (err) throw err;
+        exec(`magick convert ${list} ${outputFilePath}`,(err,stdout,stderr) => {
+            if(err) throw err
 
-            res.download(outputFilePath, (err) => {
-                if (err) throw err;
+            res.download(outputFilePath,(err) => {
+                if(err) throw err
 
-                req.files.forEach((file) => {
-                    fs.unlinkSync(file.path);
+                // delete the files which is stored
+
+                req.files.forEach(file => {
+                    fs.unlinkSync(file.path)
                 });
 
-                fs.unlinkSync(outputFilePath);
-            });
-        });
+                fs.unlinkSync(outputFilePath)
+            })
+        })
     }
-});
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
+})
+
+app.listen(PORT,() => {
+    console.log(`App is listening on Port ${PORT}`)
 })
